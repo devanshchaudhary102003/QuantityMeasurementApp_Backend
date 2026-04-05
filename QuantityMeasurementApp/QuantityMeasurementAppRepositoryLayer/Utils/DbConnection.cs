06757bb12fd9a70b092de9b2e014feac65344923
devanshchaudhary102003 <devanshchaudhary102003@gmail.com>
2026-04-05 23:05:52 +0530

    [Devansh Singh]Add. Quantity Measurement App Backend

--- QuantityMeasurementApp/QuantityMeasurementAppRepositoryLayer/Utils/DbConnection.cs
+++ QuantityMeasurementApp/QuantityMeasurementAppRepositoryLayer/Utils/DbConnection.cs
@@ -5,7 +5,7 @@ namespace QuantityMeasurementAppRepositoryLayer.Utils
     public static class DbConnectionFactory
     {
         private static readonly string connectionString =
-            "Host=ep-little-meadow-ammlx367.c-5.us-east-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_UzQ0APRmS1aH;SSL Mode=Require;Trust Server Certificate=true";
+            "Host=ep-little-meadow-ammlx367.c-5.us-east-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_UzQ0APRmS1aH;SslMode=Require;Trust Server Certificate=true";
 
         public static NpgsqlConnection CreateConnection()
         {
