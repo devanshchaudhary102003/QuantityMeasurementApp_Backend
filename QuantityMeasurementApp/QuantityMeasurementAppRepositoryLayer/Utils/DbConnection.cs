7084e7ab611c34c5004d002e37747c31fbb60fb0
devanshchaudhary102003 <devanshchaudhary102003@gmail.com>
2026-04-05 22:52:46 +0530

    [Devansh Singh]Add. Quantity Measurement App Backend

--- QuantityMeasurementApp/QuantityMeasurementAppRepositoryLayer/Utils/DbConnection.cs
+++ QuantityMeasurementApp/QuantityMeasurementAppRepositoryLayer/Utils/DbConnection.cs
@@ -1,15 +1,15 @@
-using Microsoft.Data.SqlClient;
+using Npgsql;
 
 namespace QuantityMeasurementAppRepositoryLayer.Utils
 {
     public static class DbConnectionFactory
     {
         private static readonly string connectionString =
-            "Server=localhost\\SQLEXPRESS;Database=QuantityMeasurementDB;Trusted_Connection=True;TrustServerCertificate=True";
+            "Host=ep-little-meadow-ammlx367.c-5.us-east-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_UzQ0APRmS1aH;SSL Mode=Require;Trust Server Certificate=true";
 
-        public static SqlConnection CreateConnection()
+        public static NpgsqlConnection CreateConnection()
         {
-            return new SqlConnection(connectionString);
+            return new NpgsqlConnection(connectionString);
         }
     }
 }
